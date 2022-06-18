dcdd03b010d5b7e4b92f938eef8da4388e72faac
Justin Fleming <fezzygo@gmail.com>
2022-06-18 13:17:44 -0400

    added function to create data containers for bar info (js:16:34)

--- assets/js/results.js
+++ assets/js/results.js
@@ -9,13 +9,39 @@ var obLat = '';
 var obLon = '';
 var obBase = `https://api.openbrewerydb.org/breweries?by_dist=${obLat},${obLon}`;
 
+var count = 10;
+
 
 // Get the parameters past 
-function getURLParams() {
+function getBreweryInfo() {
     
+    for (var i=0; i<count; i++){
+        var brewContainer = $("<section>");
+        brewContainer.attr("class", "container");
+        var brewName = $("<div>");
+        brewName.attr("class", "bar-info");
+        brewContainer.append(brewName);
+        var brewAddress = $("<div>");
+        brewAddress.attr("class", "bar-info");
+        brewContainer.append(brewAddress);
+        var brewPhone = $("<div>");
+        brewPhone.attr("class", "bar-info");
+        brewContainer.append(brewPhone);
+        var brewURL = $("<div>");
+        brewURL.attr("class", "bar-info");
+        brewContainer.append(brewURL);
+        searchResults.append(brewContainer);
+    }
 }
 
 function getlatlon(){
 
 }
 
+getBreweryInfo();
+            // <section class="container">
+            //     <div class="bar-name">name</div>
+            //     <div id="bar-address">address</div>
+            //     <div id="bar-phone">phone</div>
+            //     <div id="bar-website">website</div>
+            // </section>
